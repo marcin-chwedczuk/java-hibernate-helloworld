903b102db074a6afec4a994b09bc26e69cee038b
Marcin Chwedczuk <chwedczukm@yahoo.com>
2016-06-23 12:49:37 +0200

    Saving TestEntity using JPA

--- src/main/java/mc/hibernatetutorial/App.java
+++ src/main/java/mc/hibernatetutorial/App.java
@@ -1,6 +1,12 @@
 package mc.hibernatetutorial;
 
 import org.apache.log4j.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import mc.hibernatetutorial.model.*;
 
 public class App 
 {
@@ -8,6 +14,32 @@ public class App
 
     public static void main(String[] args)
     {
-        logger.debug("Hello, World!");
+        logger.debug("starting application....");
+
+        final EntityManagerFactory emf = 
+                Persistence.createEntityManagerFactory("HelloWorldPU");
+
+        // create session and save TestEntity to database
+        EntityManager entityManager = emf.createEntityManager();
+
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+
+        try {
+            TestEntity testEntity = new TestEntity();
+            testEntity.setName("super foo");    
+
+            entityManager.persist(testEntity);
+            tx.commit();
+        }
+        catch (Exception e) {
+            logger.error("cannot commit transaction", e);
+            tx.rollback();
+        }
+        finally {
+            entityManager.close();
+        }
+        
+        emf.close();
     }
 }
